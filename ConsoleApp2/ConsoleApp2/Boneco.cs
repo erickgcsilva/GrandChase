ddf2360667c3518d602f837de4080d2c0aa47579
Erick Gonçalves <93154145+erickgcsilva@users.noreply.github.com>
2022-04-27 16:06:21 -0300

    Add files via upload

---
+++ ConsoleApp2/ConsoleApp2/Boneco.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Personagem
+{
+    internal class Boneco
+    {
+        public string Nome { get; set; }
+        public int Idade { get; set; }
+        public string Classe { get; set; }
+        public string História { get; set; }
+    }
+}
